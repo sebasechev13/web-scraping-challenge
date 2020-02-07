3b62fa0ea9e29b214f18b55bb92507ac2d64ea47
Sebastian Echeverri <sebas1996e@gmail.com>
2020-02-06 19:12:39 -0500

    fixing it

---
+++ mars_scrape.py
@@ -0,0 +1,149 @@
+import pandas as pd 
+import selenium
+import splinter
+from bs4 import BeautifulSoup as bs 
+from splinter import Browser
+import requests
+import time
+import tweepy
+import json
+import re
+from tweepy import OAuthHandler
+
+import twitter_credentials
+
+def init_browser():
+    #starting the browser
+    executable_path = {'executable_path':'/usr/local/bin/chromedriver'}
+    return Browser('chrome', **executable_path, headless=True)
+
+def close_browser(browser):
+    browser.quit()
+
+def scrape_all():
+    mars = {}
+    news = scrape_news()
+    mars["news_title"] = news[0]
+    mars["mars_date"] = news[1]
+    mars["mars_paragraph"] = news[2]
+    mars["news_url"] = news[3]
+    mars['feat_img'] = f_img()
+    tweet = twitter()
+    mars['tweet_text']= tweet[0]
+    mars['tweet_url']= tweet[1]
+    
+    mars['data_table'] = table()
+    mars['hemisphere_image_urls'] = mars_h_images()
+    return  mars
+def scrape_news():
+    try:
+        ## initialize browser and latest_news dict
+        browser = init_browser()
+        
+
+
+        ## define url and command browser to visit and parse HTML for posts tag   
+        nasa_url= "https://mars.nasa.gov"
+        news_url= "https://mars.nasa.gov/news/"
+        browser.visit(news_url)
+        time.sleep(1)
+    
+        html = browser.html
+        soup = bs(html, "html.parser")
+        try:
+            #find first news article 
+            news = soup.find('div', class_='list_text')
+
+        
+            ## collect title, date, description and link of post
+            titles = news.find('div', class_='content_title').text
+            date = news.find('div', class_='list_date').text
+            news_p = news.find('div', class_='article_teaser_body').text
+            link = news.a['href']
+            ## run it untill the attribute error doesnt show up. 
+        except AttributeError:
+            print("error")
+        news_url = f'{nasa_url}{link}'
+        return titles,date,news_p,news_url
+## close browser    
+    finally:        
+        close_browser(browser)
+def f_img():
+    try:
+        browser = init_browser()
+
+        url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+        browser.visit(url2)
+
+        #get html code from it 
+        html2 = browser.html
+        soup2 = bs(html2,'html.parser')
+
+        #get the ending of the url to link the image
+        result2 = soup2.find('article', class_ = "carousel_item")
+        #identify and reutnr link to listing 
+        link = result2.a['data-fancybox-href']
+        featured_image_url = f'https://www.jpl.nasa.gov{link}'
+        return featured_image_url
+
+    finally:        
+        close_browser(browser)
+
+def twitter():
+    # set up dict
+    # set up the authication fro the twitter API
+    auth = tweepy.OAuthHandler(twitter_credentials.Cons_API_key, twitter_credentials.Cons_API_secret_key )
+    auth.set_access_token(twitter_credentials.Access_token, twitter_credentials.Access_token_secret)
+    #chose the API of twitter which we will use JSO 
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+    target_user = "MarsWxReport"
+    tweet = api.user_timeline(target_user, count =0, page = 0)
+
+    try:
+    
+        link = ((tweet)[0]['entities']['urls'][0]['url'])
+    except IndexError:
+        link="N/A"
+    
+
+    text = ((tweet)[0]['text'])
+    tweet = text.replace(link,"")
+    
+    return tweet,link
+def table():
+     #read html to get mars facts
+    mars_df = pd.read_html("https://space-facts.com/mars/")[0]
+    mars_df.columns = ['Description', 'Value']
+    mars_df.set_index("Description", inplace = True)
+    mars_facts = mars_df.to_html(index = True, header = True, classes="table table-striped")
+    return mars_facts
+def mars_h_images():
+    try:
+        browser = init_browser()
+        #go to the url to get Mars Hemisphere images 
+        url5 = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+        browser.visit(url5)
+
+        #get html code from it 
+        html2 = browser.html
+        soup5 = bs(html2, "html.parser")
+        #find all the listings in the html
+        result5 = soup5.find_all('div', class_='item')
+         #create an empty list 
+        hemisphere_image_urls =[]
+        #iterate through the four results and go to their link to get the image url
+        for result in result5:
+            link = result.a['href']
+            browser.visit('https://astrogeology.usgs.gov'+link)
+            html6 = browser.html
+            soup6 = bs(html6,'html.parser')
+            first = soup6.find('div', class_ = 'downloads')
+            image_url =first.a['href']
+            title = soup6.find('h2', class_ ='title').text
+            post = {'title': title, 'img_url': image_url}
+            hemisphere_image_urls.append(post)
+            browser.back
+        return hemisphere_image_urls
+    finally:        
+        close_browser(browser)
---
+++ twitter_credentials.py
@@ -0,0 +1,8 @@
+#credentails to access it 
+#COnsumer TOkens
+Cons_API_key= 'y8pLrhxYEVSJ8INGEOxce2Ri6'
+Cons_API_secret_key = 'YRdXDZ4pfc5g0hOJ3J5OsWSHvKokhDsr27IByMClug55fA5FKg'
+
+#Access TOkens
+Access_token  = '1225035842823114754-Gx4ZR24p0lcNKdHGesXbx0TQWQqckj'
+Access_token_secret ='G7PY1Q1H560spaJZ4VaWwKZVslc2nvPNn88Ak0NPLreTV'
